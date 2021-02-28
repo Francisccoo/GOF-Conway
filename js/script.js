1aaa6c276a578aeb83c8382d2de237b5b6f36fe8
Francisco <franchesco2009@gmail.com>
2021-02-28 18:39:03 +0100

    Añadidos botones para cambiar el color, falta revisar el código del loop

--- js/script.js
+++ js/script.js
@@ -15,6 +15,11 @@ var columnas = 100;
 var blanco = '#FFF';
 var negro = '#000';
 
+
+var rojo = '#F00';
+var azul = '#00F';
+var verde = '#0F0';
+
 function creaArray2D(f, c) {
 	var obj = new Array(f);
 	for (y = 0; y < f ; y++) {
@@ -34,6 +39,94 @@ var Agente = function(x, y, estado) {
 	this.estadoProx = this.estado //estado que tendrá el siguiente ciclo
 
 	this.vecinos = []; //guardamos el listado de sus vecinos
+
+	//Método que añade los vecinos al objeto actual
+	this.addVecinos = function() {
+		var xVecino;
+		var yVecino;
+
+		for (i = -1; i < 2; i++) {
+			for (j = -1; j < 0; j++) {
+				
+				xVecino = (this.x + j + columnas) % columnas;
+				yVecino = (this.y + i + filas) % filas;
+
+
+				//descartamos el agente actual (yo no puedo ser mi propio vecino)
+				if(i!=0 || j!=0) {
+					this.vecinos.push(tablero[yVecino][xVecino]);
+				}
+			}
+		}
+	}
+
+	this.dibuja = function(){
+
+		var color;
+
+		if(this.estado == 1) {
+
+			color = blanco;
+		}
+		else {
+			color = negro;
+		}
+
+		ctx.fillStyle = color;
+		ctx.fillRect(this.x * tileX, this.y * tileY, tileX, tileY);
+	}
+
+
+	//Programamos las leyes de Conway
+	this.nuevoCiclo = function() {
+		var suma = 0;
+
+		//calculamos la cantidad de vecinos vivos
+		for (i = 0; i < this.vecinos.length; i++) {
+			suma += this.vecinos[i].estado;
+		}
+
+		//Aplicamos las normas
+		this.estadoProx = this.estado; //por defecto se queda igual
+
+
+		//MUERTE: tiene menos de dos o más de tres
+		if(suma < 2 || suma > 3) {
+			this.estadoProx = 0;
+		}
+
+		//VIDA / REPRODUCCIÓN: tiene exactamente tres vecinos
+		if(suma == 3) {
+			this.estadoProx = 1;
+		}
+	}
+
+	this.mutacion = function(){
+		this.estado = this.estadoProx;
+	}
+
+}
+
+
+
+function inicializaTablero(obj) {
+
+	var estado;
+
+	for (var y = 0; y < filas; y++) {
+		for (var x = 0; x < columnas; x++) {
+			estado = Math.floor(Math.random()*2);
+			obj[y][x] = new Agente(x, y, estado);
+		}
+	}
+
+
+	for (var y = 0; y < filas; y++) {
+		for (var x = 0; x < columnas; x++) {
+			obj[y][x].addVecinos();
+		}
+	}
+
 }
 
 //Inicializamos todos los valores correspondientes al canvas
@@ -41,7 +134,7 @@ function inicializa() {
 
 	//Asociamos el canvas
 	canvas = document.getElementById('pantalla');
-	ctx = canvas.getContext('2D'); //Estamos indicando que vamos a trabajar con un canvas en dos dimensiones
+	ctx = canvas.getContext('2d'); //Estamos indicando que vamos a trabajar con un canvas en dos dimensiones
 
 
 	//Ajustamos el tamaño del canvas
@@ -55,8 +148,40 @@ function inicializa() {
 	//Creamos el tablero
 	tablero = creaArray2D(filas, columnas);
 
+	//Lo inicializamos
+	inicializaTablero(tablero);
+
 	//Ejecutamos el bucle principal cada 33,33 milisegundos
 	setInterval(function(){principal();}, 1000/fps);
+
+	
+}
+
+
+function dibujaTablero(obj) {
+
+
+	//Dibuja los agentes
+	for (y = 0; y < filas ; y++) {
+		for (x = 0; x < columnas ; x++) {
+			obj[y][x].dibuja();
+		}
+	}
+
+	//Calcula el siguiente ciclo
+	for (y = 0; y < filas ; y++) {
+		for (x = 0; x < columnas ; x++) {
+			obj[y][x].nuevoCiclo();
+		}
+	}
+
+
+	//Aplica la mutacion
+	for (y = 0; y < filas ; y++) {
+		for (x = 0; x < columnas ; x++) {
+			obj[y][x].mutacion();
+		}
+	}
 }
 
 function borraCanvas() {
@@ -65,6 +190,29 @@ function borraCanvas() {
 }
 
 function principal() {
-	console.log('fotograma');
 	borraCanvas();
+	dibujaTablero(tablero);
+}
+
+
+	
+//Cambiar colores
+
+function negroArojo() {
+
+	negro = rojo;
+
+}
+
+function negroAazul() {
+
+	negro = azul;
+
+}
+
+
+function negroAverde() {
+
+	negro = verde;
+
 }
